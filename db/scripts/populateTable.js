1e5c58ad1b5cb43dad574ceae536a83944e56102
Hazieon <handrew3721@gmail.com>
2021-02-26 21:26:26 +0000

    FEATURE: populateTable script working

--- db/scripts/populateTable.js
+++ db/scripts/populateTable.js
@@ -8,7 +8,15 @@ const presetData = [
     location: { lat: -1.2884, lng: 36.8233 },
     address: "Nairobi, City Square, Nairobi, Kenya",
     date: "2021-01-29 21:30:00",
-    details: {},
+    details: {
+      free: true,
+      "Green space": true,
+      "Dog friendly": true,
+      "Near shops": true,
+      "Public bathrooms": true,
+      "Flat parking": true,
+      "Away from traffic": true,
+    },
   },
   {
     uuid: uuidv4(),
@@ -16,38 +24,58 @@ const presetData = [
     address:
       "Rua Doutor João de Deus, 28, 60040-350, Fortaleza Fátima Fortaleza, Brasil",
     date: "2020-12-21 11:30:00",
-    details: {},
+    details: {
+      free: true,
+      "Green space": true,
+      "Dog friendly": true,
+      "Near shops": true,
+      "Near petrol stn": true,
+      "Away from traffic": true,
+    },
   },
   {
     uuid: uuidv4(),
     location: { lat: 52.52011994421292, lng: -1.4640778962357217 },
     address: "Nuneaton, CV11, England United Kingdom",
     date: "2021-1-29 17:00:00",
-    details: {},
+    details: {
+      free: true,
+      "Flat parking": true,
+      "Green space": true,
+      "Near petrol stn": true,
+      "Near shops": true,
+      "Public bathrooms": true,
+      "Safe/Secure": true,
+    },
   },
   {
     uuid: uuidv4(),
     location: { lat: 52.0507548306133, lng: -1.7856869475872172 },
     address: "Back Ends, Chipping Campden, GL55 6AA, England United Kingdom",
     date: "2020-9-20 17:00:00",
-    details: {},
+    details: {
+      free: true,
+      Stealthy: true,
+      "Green space": true,
+      "Dog friendly": true,
+      "Near shops": true,
+      "Safe/Secure": true,
+      "Away from traffic": true,
+    },
   },
 ];
 
-const text = `INSERT INTO session(uuid, location, responses, outcome, question, coach, date, throwaway) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`;
+const sql = `INSERT INTO vancations(uuid, location, address, date, details) VALUES($1, $2, $3, $4, $5) RETURNING *`;
 
 const populateTable = async () => {
-  for (let i = 0; i < userData.length; i++) {
-    let currentItem = userData[i];
-    let res = await query(text, [
-      currentItem.uuid,
-      currentItem.participants,
-      currentItem.responses,
-      currentItem.outcome,
-      currentItem.question,
-      currentItem.coach,
-      currentItem.date,
-      currentItem.throwaway,
+  for (let i = 0; i < presetData.length; i++) {
+    let current = presetData[i];
+    let res = await query(sql, [
+      current.uuid,
+      current.location,
+      current.address,
+      current.date,
+      current.details,
     ]);
     console.log(res.rows);
   }
