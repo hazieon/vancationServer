448bce022f09046bc0493f2f460dbf92e69a02d7
Hazieon <handrew3721@gmail.com>
2021-02-27 21:21:35 +0000

    update for CORS issue

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var cors = require("cors");
+
 var bodyParser = require('body-parser')
 
 var indexRouter = require("./routes/index");
 // var usersRouter = require("./routes/users");
-app.use(cors());
+var cors = require("cors");
 var app = express();
+
+// Handle cors
+app.use(cors());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
